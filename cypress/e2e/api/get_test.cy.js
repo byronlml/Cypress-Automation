fe81cd38a0125b83f7121cf2740befa8a81976d4
@Bayron.Orozco <bayron.orozco@millicom.com>
2023-12-15 18:02:48 -0500

    api testing added

---
+++ cypress/e2e/api/get_test.cy.js
@@ -0,0 +1,22 @@
+describe("API pública", () => {
+
+  const url = 'https://randomuser.me'
+
+  it("devuelve una lista de APIs", () => {
+    // Inicializa la API
+    cy.request(url + "/api/").then((res) => {
+      const data = JSON.stringify(res.body);
+
+    // Extrae el objeto de resultados
+      const results = JSON.parse(data).results[0];
+
+    // Comprueba que el género sea "male"
+      cy.expect(results.gender).to.equal("male");
+      // Verifica que la respuesta tenga éxito
+      cy.expect(res.status).to.eq(200);
+
+    });
+
+  });
+});
+  
---
+++ cypress/e2e/api/jsonplaceholder_test.cy.js
@@ -0,0 +1,40 @@
+describe("API jsonplaceholder", () => {
+
+    const url = 'https://jsonplaceholder.typicode.com';
+    const dataPath = "cypress/fixtures/create_user.json";
+  
+    it("GET devuelve una lista de APIs", () => {
+      // Inicializa la API
+      cy.request(url + "/users").then((res) => {
+        const data = JSON.stringify(res.body);
+
+        // Accede al primer objeto del array
+        const user = JSON.parse(data)[0];
+
+        // Comprueba que el nombre sea "Leanne Graham"
+        cy.expect(user.name).to.equal("Leanne Graham");
+        // Verifica que la respuesta tenga éxito
+        cy.expect(res.status).to.eq(200);
+  
+      });
+    });
+
+    it("crea un nuevo post", () => {
+        // Obtiene los datos del JSON
+        cy.readFile(dataPath).then((data) => {
+          // Realiza la petición POST
+          cy.request({
+            method: 'POST',
+            url: url + "/users", 
+            body: data,
+          }).then((res) => {
+            const test = JSON.stringify(res.body);
+            cy.log(test)
+            cy.wait(30000)
+            // Verifica que la respuesta tenga éxito
+            cy.expect(res.status).to.eq(201);
+
+          });
+        });
+      });
+  });
